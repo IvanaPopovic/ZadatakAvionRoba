2a90a89483f2ddc11864e0b4b264f2ffcd704d07
IvanaPopovic <2106ivana@gmail.com>
2016-10-01 18:49:51 +0200

    Uradjena klasa Avion fixes #3

--- src/main/java/model/Avion.java
+++ src/main/java/model/Avion.java
@@ -1,7 +1,87 @@
 package model;
+import com.j256.ormlite.dao.ForeignCollection;
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.field.ForeignCollectionField;
+import com.j256.ormlite.table.DatabaseTable;
+
+import java.util.Date;
 
 /**
  * Created by android on 1.10.16..
  */
+@DatabaseTable(tableName = "avion")//Anotacija za tabelu avion
 public class Avion {
+    //Staticki atributi
+    public static final String POLJE_OZNAKA="oznaka";
+    public static final String POLJE_RASPON_KRILA="raspon_krila";
+
+
+    //atributi za upisu u bazu
+    @DatabaseField(generatedId = true)//primarni kljuc koji se automatski generise
+    private int id;
+    @DatabaseField(columnName = POLJE_OZNAKA)
+    private String oznaka;
+    @DatabaseField(columnName = POLJE_RASPON_KRILA)
+    private int rasponKrila;
+
+    //Atribut za vise kraj veze izmendju klasa Avion i Roba
+    @ForeignCollectionField(foreignFieldName = "avion")
+    private ForeignCollection<Roba> roba;
+
+    //Konstrutkor bez parametara
+    public Avion(){
+        //Obavezan za potrebe ORMLite-a
+    }
+
+    //Konstrutkor koji ocekuje parametre naslov, brojStrana i datumIzdanja
+    public Avion(String oznaka, int rasponKrila) {
+        this.oznaka = oznaka;
+        this.rasponKrila = rasponKrila;
+    }
+
+    //get i set metode
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getOznaka() {
+        return oznaka;
+    }
+
+    public void setOznaka(String oznaka) {
+        this.oznaka= oznaka;
+    }
+
+    public int getRasponKrila() {
+        return rasponKrila;
+    }
+
+    public void setRasponKrila(int rasponKrila) {
+        this.rasponKrila = rasponKrila;
+    }
+
+
+    public ForeignCollection<Roba> getRoba() {
+        return roba;
+    }
+
+    public void setRoba(ForeignCollection<Roba> roba) {
+        this.roba = roba;
+    }
+
+    //Redefinisana metoda toString
+    @Override
+    public String toString() {
+        return "Avion{" +
+                "id='" + id + '\'' +
+                ", oznaka=" + oznaka +
+                ", rasponKrila=" + rasponKrila +
+                '}';
+    }
+
+
 }
